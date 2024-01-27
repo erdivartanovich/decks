c1ead6bd3bcef5146c7b4b3c73db675b92adb268
erdivartanovich <erdivartanovich@gmail.com>
2024-01-28 00:43:24 +0800

    fix file permissions

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    error_log('testing');
+    Log::info('This message will be logged to the Laravel log.');
+    Log::error('This error will be logged to the Laravel log.');
     return view('welcome');
 });
---
+++ run.sh
@@ -0,0 +1,3 @@
+export MYSQL_USER_PASSWORD=password123
+export MYSQL_ROOT_PASSWORD=rootpassword123
+docker-compose up -d --build
