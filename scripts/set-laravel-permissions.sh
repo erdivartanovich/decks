e051e6f3ceb3585cefa82f16819165432d7804af
erdivartanovich <erdivartanovich@gmail.com>
2024-01-31 02:34:10 +0800

    fix laravel permission script

--- scripts/set-laravel-permissions.sh
+++ scripts/set-laravel-permissions.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env sh
 
+if [ ! "$1" ]; then
+    echo "Error. Please define the project root directory!"
+    echo "List of available projects:"
+    ls -1
+    exit 1
+fi
 # give host user permissions
-WORKDIR="${1:-.}"
-sudo chown -R 1000:www-data "$WORKDIR"
-sudo find "$WORKDIR" -type f -exec chmod 664 {} \;
-sudo find "$WORKDIR" -type d -exec chmod 775 {} \;
+sudo chown -R 1000:www-data "$1"
+sudo find "$1" -type f -exec chmod 664 {} \;
+sudo find "$1" -type d -exec chmod 775 {} \;
 
-# give web-server permissions
-sudo chgrp -R www-data storage "$WORKDIR"/bootstrap/cache
-sudo chmod -R ug+rwx storage "$WORKDIR"/bootstrap/cache
+#give web-server permissions
+sudo chgrp -R www-data "$1"/storage "$1"/bootstrap/cache
+sudo chmod -R ug+rwx "$1"/storage "$1"/bootstrap/cache
